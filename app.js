42a79d044348b8354fe684cc6787014338f92cb0
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-01-21 18:29:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get("/", (req, res) => {
     res.send("Welcome to the home page");
 });
 
-app.listen(3000, () => {
+let PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
     console.log("Started at port 3000");
 });
